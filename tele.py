a5706f523bff4a62c24d3bfaddbd206559f95ace
Ashwinhacker <102268450+Ashwinhacker@users.noreply.github.com>
2024-09-29 09:40:54 +0530

    tele.py

--- tele.py
+++ tele.py
@@ -4,19 +4,22 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters
 
 BOT_TOKEN = "7703670023:AAEjsUcSLAxTZr_n2VO8A8FhSZoTbOS0xx0"
 
-async def reply(update:Update,context) -> None:
+async def reply(update: Update, context) -> None:
     user_message = update.message.text
     print(user_message)
-    result = subprocess.check_output(user_message,shell=True, text=True)
-    print(result)
-    await update.message.replytext(f"shell output:{result}")
+    
+    try:
+        result = subprocess.check_output(user_message, shell=True, text=True)
+        print(result)
+        await update.message.reply_text(f"Shell output: {result}")
+    except subprocess.CalledProcessError as e:
+        print(e)
+        await update.message.reply_text(f"Error: {e}")
 
 def main() -> None:
-    app = ApplicationBuilder.builder().token(BOT_TOKEN).bulid()
-    app.add_handler(MessageHandler(filter.TEXT, reply))
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT, reply))
     app.run_polling()
 
 if __name__ == "__main__":
     main()
-
-
