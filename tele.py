4cb1c73f59c0980fd467e0a0c675ed97e337ba6d
Ashwinhacker <102268450+Ashwinhacker@users.noreply.github.com>
2024-09-29 11:19:19 +0530

    tele.py

--- tele.py
+++ tele.py
@@ -2,19 +2,27 @@ import subprocess
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters
 
-BOT_TOKEN = "7703670023:AAEjsUcSLAxTZr_n2VO8A8FhSZoTbOS0xx0"
+BOT_TOKEN = [""] #use you Token 
+
+ALLOWED_COMMANDS = ['ls', 'pwd', 'whoami', 'uptime', 'df', 'free', 'apt', 'install', 'get', 'git clone' , 'ping' ,'cls', 'dir', 'cat','clear' ]
 
 async def reply(update: Update, context) -> None:
-    user_message = update.message.text
-    print(user_message)
-    
-    try:
-        result = subprocess.check_output(user_message, shell=True, text=True)
-        print(result)
-        await update.message.reply_text(f"Shell output: {result}")
-    except subprocess.CalledProcessError as e:
-        print(e)
-        await update.message.reply_text(f"Error: {e}")
+    user_message = update.message.text.strip().split()  
+    command = user_message[0]  
+
+    if command in ALLOWED_COMMANDS:
+        try:
+            result = subprocess.check_output(user_message, shell=False, text=True)
+            print(result)
+            await update.message.reply_text(f"Shell output:\n{result}")
+        except subprocess.CalledProcessError as e:
+            print(f"Command error: {e}")
+            await update.message.reply_text(f"Error: {e}")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            await update.message.reply_text(f"An unexpected error occurred: {e}")
+    else:
+        await update.message.reply_text(f"Command '{command}' is not allowed.")
 
 def main() -> None:
     app = ApplicationBuilder().token(BOT_TOKEN).build()
